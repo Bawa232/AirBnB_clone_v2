7ee6593595e76148e50aee1dac94e7fe5671d69d
James Bawa <jbawa032@gmail.com>
2023-10-08 12:57:07 +0100

    more fixed

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
-'''flask app that prints hello worldls'''
+'''flask app that prints hello hello hbnb'''
+
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
-    return 'Hello HBNB!ce'
+    return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
